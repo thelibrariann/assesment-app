d9c20e39dce46c899d48ac820a7453825bd7ad0c
thelibrariann <fabian.gonzalez@gmail.com>
2023-07-21 22:09:26 -0400

    first commit

---
+++ src/main/java/com/assesment/app/AssementApplication.java
@@ -0,0 +1,13 @@
+package com.assesment.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assesment/app/AssementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assesment.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
